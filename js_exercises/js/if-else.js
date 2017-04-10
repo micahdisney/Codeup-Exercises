ec7ea4495cca3c74dd183250e4722e0098bbf545
micahdisney <micah.n.disney@gmail.com>
2017-04-10 14:38:33 -0500

    -created if-else-js.html and if-else.js

---
+++ js_exercises/js/if-else.js
@@ -0,0 +1,58 @@
+/**
+ * Created by micahdisney on 4/10/17.
+ */
+"use strict";
+
+
+var avg = (70 + 80 + 95) / 3;
+
+if (avg >= 80){
+    console.log("You're awesome!");
+} else {
+    console.log("You need practice");
+
+
+
+
+
+var customer = "";
+var costDollars = null;
+var discountRate = .35;
+var totalSpentDollars = null;
+var discountBreakpointDollars = 200;
+var cameronSpentDollars = 180;
+var ranSpentDollars = 250;
+var georgeSpentDollars = 320;
+
+
+
+customer = cameron;
+totalSpentDollars = cameronSpentDollars;
+
+if (totalSpentDollars > discountBreakpointDollars) {
+    costDollars = totalSpentDollars - (totalSpentDollars * discountRate);
+} else {
+    costDollars = totalSpentDollars;
+}
+
+
+console.log(customer + " bought " + "$" + totalSpentDollars + " worth of products." + " Final payment: " + "$" + costDollars + ".");
+
+
+
+
+
+
+
+var flipACoin = Math.floor(Math.random()* 2)
+
+if (flipACoin === 0){
+    console.log("Buy a car");
+}
+else {
+    console.log("Buy a house");
+}
+
+
+var choice = (flipACoin) ?"Buy a house" : "Buy a car";
+console.log(choice);
